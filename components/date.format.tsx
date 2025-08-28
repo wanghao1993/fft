cd0555fc01e52d44cef49bfdffbc84f3ed65272b
wanghao1993 <whao53333@gmail.com>
2025-08-28 16:07:18 +0800

    feat: formate date

--- components/date.format.tsx
+++ components/date.format.tsx
@@ -3,13 +3,16 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import "dayjs/locale/zh-cn";
 import "dayjs/locale/en";
 import { getLocale } from "next-intl/server";
+import timezone from "dayjs/plugin/timezone";
 
+dayjs.extend(timezone);
 dayjs.extend(relativeTime);
 
 export async function DateFormatFromNow(date: number) {
   const locale = await getLocale();
   return dayjs(date * 1000)
     .locale(locale)
+    .tz("Asia/Shanghai")
     .fromNow();
 }
 
@@ -17,5 +20,6 @@ export async function DateFormat({ date }: { date: number }) {
   const locale = await getLocale();
   return dayjs(date * 1000)
     .locale(locale)
+    .tz("Asia/Shanghai")
     .format("YYYY-MM-DD HH:mm:ss");
 }
